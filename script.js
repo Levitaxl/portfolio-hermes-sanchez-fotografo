c110552e87dbb8cc9778d6c0a44a857d1603a84c
Levitaxl <hermessamuel10@gmail.com>
2026-06-07 22:35:05 -0400

    los rostros detras del mito

--- script.js
+++ script.js
@@ -53,7 +53,8 @@ const lightbox = document.getElementById('lightbox');
 const seriesOrder = [
     { url: "peso-del-aire-en-la-habitacion.html", title: "El peso del aire en la habitación" },
     { url: "caminando-por-ahi.html", title: "Caminando por ahí" },
-    { url: "gritos-de-oracion.html", title: "Gritos de oración" }
+    { url: "gritos-de-oracion.html", title: "Gritos de oración" },
+    { url: "los-rostros-detras-de-mito.html", title: "Los Rostros detras del Mito" }
 ];
 
 function generateSeriesNav() {
